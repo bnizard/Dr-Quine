8277518f82c09d5aac7ff11c6b97f9b6314b06c7
Bastien NIZARD <bnizard@e3r9p5.42.fr>
2016-09-27 17:41:54 +0200

    ok

--- Grace.c
+++ Grace.c
@@ -1,5 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
-#define FT(x)int main(){FILE* fichier=NULL;char *a="#include%c<stdlib.h>%c#include%c<stdio.h>%c#define%cFT(x)int%cmain(){FILE*%cfichier=NULL;char%c*a=%c%s%c;fichier=fopen(%cGrace_kid.c%c, %cw%c);if%c(fichier!=NULL)%cfprintf(fichier,a,32,10,32,10,32,32,32,32,34,a,34,34,34,34,34,32,32,32,10,10);return%c0;}%c%cFT(x);";fichier=fopen("Grace_kid.c", "w");if (fichier!=NULL) fprintf(fichier,a,32,10,32,10,32,32,32,32,34,a,34,34,34,34,34,32,32,32,10,10);return 0;}
-
+#define FT(x)int main(){FILE* fichier=NULL;char *a="#include%c<stdlib.h>%c#include%c<stdio.h>%c#define%cFT(x)int%cmain(){FILE*%cfichier=NULL;char%c*a=%c%s%c;fichier=fopen(%cGrace_kid.c%c, %cw%c);if%c(fichier!=NULL)%cfprintf(fichier,a,32,10,32,10,32,32,32,32,34,a,34,34,34,34,34,32,32,32,10,32,32,10,32,32,10,10,10,10);return%c0;}%c#define%cANNEE%c2016%c#define%cMOIS%cSEPTEMBRE%c/*%cCommentaire%c*/%cFT(x);";fichier=fopen("Grace_kid.c", "w");if (fichier!=NULL) fprintf(fichier,a,32,10,32,10,32,32,32,32,34,a,34,34,34,34,34,32,32,32,10,32,32,10,32,32,10,10,10,10);return 0;}
+#define ANNEE 2016
+#define MOIS SEPTEMBRE
+/*
+Commentaire
+*/
 FT(x);
