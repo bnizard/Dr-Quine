796b43e10e6669176735bf43eeb7f228abd5e65d
Bastien NIZARD <bnizard@e1r10p5.42.fr>
2016-10-20 17:10:51 +0200

    Sullydefine

--- Sully.c
+++ Sully.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 #define I 5
+#define NAME "Sully_4"
+#define NAMEC "Sully_4.c"
+#define GCC "/usr/bin/gcc -o Sully_4 Sully_4.c"
 
-int main(void){int i=I;if(i>0){char *name,*gcc;sprintf(name,"Sully_%d",--i);char *a="#include%c<stdlib.h>%c#include%c<stdio.h>%c#define%cI%c%d%c%cint%cmain(void)%cif(i>0){char*%cname;sprintf(name,%cSully_%d.c%c,--i);char%c*a=%c%s%c;FILE*%cfichier=NULL;fichier=fopen(name,%cw%c);if(fichier!=NULL)fprintf(fichier,a,32,10,32,10,32,32,i,10,10,32,32,32,34,i,34,32,34,a,34,32,34,34,10);}return(0);}%c";FILE* fichier=NULL;fichier=fopen(strcat(name, ".c"),"w");if(fichier!=NULL)fprintf(fichier,a,32,10,32,10,32,32,i,10,10,32,32,32,34,i,34,32,34,a,34,32,34,34,10);sprintf(gcc,"/usr/bin/gcc -o %s %s.c",name,name);}return(0);}
+int main(void){int i=I;if(i>0){char *a="#include%c<stdlib.h>%c#include%c<stdio.h>%c#define%cI%c%d%c#define%cNAME%c%cSully_%d%c%c#define%cNAMEC%c%cSully_%d.c%c%c#define%cGCC%c%c/usr/bin/gcc%c-o%cSully_%d%cSully_%d.c%c%c%c";FILE* fichier=NULL;fichier=fopen(NAMEC,"w");if(fichier!=NULL)fprintf(fichier,a,32,13,32,13,32,32,i-1,13,32,32,34,i-1,34,13,32,32,34,i-1,34,13,32,32,34,32,32,i-1,32,i-1,34,13,13);fclose(fichier);system(GCC);}return(0);}
