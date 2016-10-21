6c58253741481c47d8c219917380aa2a9a12a86a
Bastien NIZARD <bnizard@e1r9p5.42.fr>
2016-10-21 16:12:46 +0200

    ok

--- Sully.c
+++ Sully.c
@@ -1,8 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#define I 15
-#define NAME "Sully_15"
-#define NAMEC "Sully_15.c"
-#define GCC "/usr/bin/gcc -o Sully_15 Sully_15.c && ./Sully_15"
-
-int main(void){int i=I;if(i>0){char *a="#include%c<stdlib.h>%c#include%c<stdio.h>%c#define%cI%c%d%c#define%cNAME%c%cSully_%d%c%c#define%cNAMEC%c%cSully_%d.c%c%c#define%cGCC%c%c/usr/bin/gcc%c-o%cSully_%d%cSully_%d.c%c&&%c./Sully_%d%c%c%cint%cmain(void){int%ci=I;if(i>0){char%c*a=%c%s%c;FILE*%cfichier=NULL;fichier=fopen(NAMEC,%cw%c);if(fichier!=NULL)fprintf(fichier,a,32,13,32,13,32,32,i-1,13,32,32,34,i-1,34,13,32,32,34,i-1,34,13,32,32,34,32,32,i-1,32,i-1,32,32,i-1,34,13,13,32,32,32,34,a,34,32,34,34);fclose(fichier);system(GCC);}return(0);}";FILE* fichier=NULL;fichier=fopen(NAMEC,"w");if(fichier!=NULL)fprintf(fichier,a,32,13,32,13,32,32,i-1,13,32,32,34,i-1,34,13,32,32,34,i-1,34,13,32,32,34,32,32,i-1,32,i-1,32,32,i-1,34,13,13,32,32,32,34,a,34,32,34,34);fclose(fichier);system(GCC);}return(0);}
+#include <string.h>
+#define I 5
+int main(int argc, char **argv){int i;if(!(strcmp(argv[0], "./Sully")))i=I+1;else i=I;if(i>0){char name[40],namec[40],exec[40],cmd[100];sprintf(name,"Sully_%d",i-1);sprintf(namec,"%s.c",name);sprintf(exec,"./%s",name);char *a="#include%c<stdlib.h>%c#include%c<stdio.h>%c#include%c<string.h>%c#define%cI%c%d%cint%cmain(int%cargc,%cchar%c**argv){int%ci;if(!(strcmp(argv[0],%c%c./Sully%c)))i=I+1;else%ci=I;if(i>0){char%cname[40],namec[40],exec[40],cmd[100];sprintf(name,%cSully_%cd%c,i-1);sprintf(namec,%c%cs.c%c,name);sprintf(exec,%c./%cs%c,name);char%c*a=%c%s%c;FILE*%cfichier=NULL;fichier=fopen(namec,%cw%c);if(fichier!=NULL)fprintf(fichier,a,32,10,32,10,32,10,32,32,i-1,10,32,32,32,32,32,32,34,34,32,32,34,37,34,34,37,34,34,37,34,32,34,a,34,32,34,34,34,32,32,37,32,37,32,32,37,34);fclose(fichier);sprintf(cmd,%c/usr/bin/gcc%c-o%c%cs%c%cs%c&&%c%cs%c,name,namec,exec);system(cmd);}return(0);}";FILE* fichier=NULL;fichier=fopen(namec,"w");if(fichier!=NULL)fprintf(fichier,a,32,10,32,10,32,10,32,32,i-1,10,32,32,32,32,32,32,34,34,32,32,34,37,34,34,37,34,34,37,34,32,34,a,34,32,34,34,34,32,32,37,32,37,32,32,37,34);fclose(fichier);sprintf(cmd,"/usr/bin/gcc -o %s %s && %s",name,namec,exec);system(cmd);}return(0);}
