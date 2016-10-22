7c6b74def5fa793e93af6dad72cf380221891619
Bastien NIZARD <bnizard@e1r9p5.42.fr>
2016-10-22 16:28:22 +0200

    Python

--- Python/Grace.py
+++ Python/Grace.py
@@ -1,4 +1,5 @@
-a = "a = %c%s%c%cbuf = a %c (34,a,34,10,37,10,34,34,34,34,10)%cfichier = open(%cGrace_kid.py%c,%cw%c)%cfichier.write(buf)"
-buf = a % (34,a,34,10,37,10,34,34,34,34,10)
+a = "a = %c%s%c%cbuf = a %c (34,a,34,10,37,10,34,34,34,34,10,10)%cfichier = open(%cGrace_kid.py%c,%cw%c)%cfichier.write(buf)%c#Commentaire"
+buf = a % (34,a,34,10,37,10,34,34,34,34,10,10)
 fichier = open("Grace_kid.py","w")
 fichier.write(buf)
+#Commentaire
